1c27c5e4f88312b78bf2e4a9561a9f9cb70b626d
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-13 13:35:20 +0300

    Add products and categories views

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<h1>Products Page Test</h1>
+@foreach ($products as $product)
+    <div>{{ $product->name }} - {{ $product->price }}</div>
+@endforeach
+
 <html>
 <head>
     <meta charset="UTF-8">
