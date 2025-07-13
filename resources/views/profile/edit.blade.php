b875651cddedb98f0d9e12e9836fb1c5c6ec2caf
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-14 01:10:37 +0300

    resolved merge conflict in resources\views\profile\edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,11 +1,29 @@
-<form action="{{ route('categories.update', $category->id) }}" method="POST">
-    @csrf
-    @method('PUT')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
 
-    <div class="form-group">
-        <label>اسم التصنيف</label>
-        <input type="text" name="name" value="{{ $category->name }}" class="form-control" required>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-profile-information-form')
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-password-form')
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.delete-user-form')
+                </div>
+            </div>
+        </div>
     </div>
-
-    <button type="submit" class="btn btn-primary">تحديث</button>
-</form>
+</x-app-layout>
