96040e881abbe600ff9d2cecf7bba2b7c1e564ca
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-13 13:20:01 +0300

    Add products route and view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,25 +1,24 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-  <h1>المنتجات</h1>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>الاسم</th>
-        <th>السعر</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($products as $product)
-      <tr>
-        <td>{{ $product->id }}</td>
-        <td>{{ $product->name }}</td>
-        <td>{{ $product->price }}</td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>قائمة المنتجات</title>
+</head>
+<body>
+    <h1>المنتجات</h1>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>الاسم</th>
+            <th>السعر</th>
+            <th>الفئة</th>
+        </tr>
+        @foreach($products as $product)
+        <tr>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->price }}</td>
+            <td>{{ $product->category_id }}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
