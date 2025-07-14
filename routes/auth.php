b908f08d2b3ea20abefd75b8617f2c33653a5605
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-14 14:06:18 +0300

    resolved merge conflict in C:\Users\abdrh\Desktop\card\store\resources\views\categories\list.blade.php

--- routes/auth.php
+++ routes/auth.php
@@ -18,9 +18,11 @@ Route::middleware('guest')->group(function () {
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+                ->name('login')
+                ->middleware('guest');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
@@ -55,5 +57,7 @@ Route::middleware('auth')->group(function () {
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
+                ->name('logout')
+                ->middleware('guest');
+
 });
