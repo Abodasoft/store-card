e742d01faceedb6baca2cf6a8871ce64acd96093
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-15 01:05:02 +0300

    product.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,17 +1,8 @@
+<?php
+
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-
-class StoreController extends Controller
+abstract class Controller
 {
-    public function index()
-    {
-        // جلب جميع التصنيفات مع منتجاتها النشطة فقط
-        $categories = Category::where('is_active', true)
-            ->with(['products' => function($q) {
-                $q->where('is_active', true);
-            }])->get();
-
-        return view('store.index', compact('categories'));
-    }
+    //
 }
