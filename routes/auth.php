ce845e06f6da104f5a06b6e7dc241c30d3d54c2e
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-14 14:53:27 +0300

    resolved merge conflict in C:\Users\abdrh\Desktop\card\store\resources\views\categories\list.blade.php

--- routes/auth.php
+++ routes/auth.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return 'Test route working ✅';
+});
+
 /*
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
