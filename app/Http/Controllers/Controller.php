35a59b9ed4347207e767d705d27ba6f985b4697b
Abodasoft <abdrhman124barkat@gmail.com>
2025-07-14 12:25:55 +0300

    resolved merge conflict in C:\Users\abdrh\Desktop\card\store\resources\views\categories\list.blade.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function showProducts(Category $category)
+{
+    $products = $category->products;
+    return view('categories.products', compact('category', 'products'));
+}
 }
